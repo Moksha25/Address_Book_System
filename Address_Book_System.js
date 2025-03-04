18a7bbbace66a6e93357d5f95e5d6b748b947fcb
Mokshini Baglekar <baglekarmokshini03@gmail.com>
2025-03-04 16:24:18 +0530

    [Mokshini Baglekar] Modify changes: Ability to get numbers of contact person count by city or state.

--- Address_Book_System.js
+++ Address_Book_System.js
@@ -99,21 +99,44 @@ function searchByCityOrState(city, state) {
 }
 
 function viewByCityOrState(city, state) {
-    let result = addressBook.filter(contact => contact.city === city || contact.state === state)
+    return addressBook
+        .filter(contact => contact.city === city || contact.state === state)
         .map(contact => `${contact.firstName} ${contact.lastName}`);
-    return result;
+}
+
+function countByCityOrState() {
+    let cityCount = addressBook.reduce((acc, contact) => {
+        acc[contact.city] = (acc[contact.city] || 0) + 1;
+        return acc;
+    }, {});
+
+    let stateCount = addressBook.reduce((acc, contact) => {
+        acc[contact.state] = (acc[contact.state] || 0) + 1;
+        return acc;
+    }, {});
+
+    return { cityCount, stateCount };
 }
 
 try {
     let contact1 = new Contact("Mokshini", "Baglekar", "Ahinsa vihar", "Bhopal", "Madhya Pradesh", "400001", "9301000083", "mokshini.baglekar@gmail.com");
     let contact2 = new Contact("Bhavesh", "Malviya", "Vijay nagar", "Indore", "Madhya Pradesh", "110001", "7690000686", "bhavesh.malviya@gmail.com");
+    let contact3 = new Contact("Rohan", "Sharma", "Kolar", "Bhopal", "Madhya Pradesh", "400002", "9826000011", "rohan.sharma@gmail.com");
+    let contact4 = new Contact("Aisha", "Khan", "Hinjewadi", "Pune", "Maharashtra", "411057", "9922000022", "aisha.khan@gmail.com");
+
     addContact(contact1);
     addContact(contact2);
+    addContact(contact3);
+    addContact(contact4);
 
     displayContacts();
 
     console.log("Contacts in Bhopal:", viewByCityOrState("Bhopal", ""));
     console.log("Contacts in Madhya Pradesh:", viewByCityOrState("", "Madhya Pradesh"));
+
+    let { cityCount, stateCount } = countByCityOrState();
+    console.log("Count by City:", cityCount);
+    console.log("Count by State:", stateCount);
 } catch (error) {
     console.error(error.message);
 }
